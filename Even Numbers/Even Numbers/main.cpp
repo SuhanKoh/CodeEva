fc105a97f7414c3a74464c65e175473a03145ea1
Suhan <suhankoh@mail.sfsu.edu>
2014-12-29 14:29:36 -0800

    Update Even Number challenge.

--- Even Numbers/Even Numbers/main.cpp
+++ Even Numbers/Even Numbers/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
-
+#include <fstream>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream IS(argv[1], ios::in);
+    string line;
+    while(getline(IS, line)){
+        string::reverse_iterator line2 = line.rbegin(); // reverse the line
+        if(line2[0]%2 == 0){
+            cout << "1" << endl;
+        }else{
+            cout << "0" << endl;
+        }
+    }
     return 0;
 }
